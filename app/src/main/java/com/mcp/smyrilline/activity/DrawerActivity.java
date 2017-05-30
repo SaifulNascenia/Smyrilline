a3b39daeb47d7fe1998b6e223243df79fc0b47b3
saiful <saiful@bdipo.com>
2017-05-30 17:35:17 +0600

    map drawer and fragment class

--- app/src/main/java/com/mcp/smyrilline/activity/DrawerActivity.java
+++ app/src/main/java/com/mcp/smyrilline/activity/DrawerActivity.java
@@ -147,18 +147,16 @@ public class DrawerActivity extends AppCompatActivity implements BleStateListene
 
         mDrawerListItems = new ArrayList<>();
         mDrawerListItems.add(new DrawerItem(getString(R.string.view_booking), R.drawable.ic_grid_ticket_white));
-        mDrawerListItems.add(new DrawerItem(getString(R.string.duty_free), R.drawable.ic_grid_duty_free_white));
-        mDrawerListItems.add(new DrawerItem(getString(R.string.coupons), R.drawable.ic_grid_coupons_white));
-        mDrawerListItems.add(new DrawerItem(getString(R.string.restaurants), R.drawable.ic_grid_restaurants_white));
         mDrawerListItems.add(new DrawerItem(getString(R.string.ship_tracker), R.drawable.ic_grid_ship_tracker_white));
         mDrawerListItems.add(new DrawerItem(getString(R.string.inbox), R.drawable.ic_grid_inbox_white));
+        mDrawerListItems.add(new DrawerItem(getString(R.string.duty_free), R.drawable.ic_grid_duty_free_white));
+        mDrawerListItems.add(new DrawerItem(getString(R.string.restaurants), R.drawable.ic_grid_restaurants_white));
         mDrawerListItems.add(new DrawerItem(getString(R.string.destinations), R.drawable.ic_grid_destinations_white));
+        mDrawerListItems.add(new DrawerItem(getString(R.string.coupons), R.drawable.ic_grid_coupons_white));
         mDrawerListItems.add(new DrawerItem(getString(R.string.settings), R.drawable.ic_grid_settings_white));
         mDrawerListItems.add(new DrawerItem(getString(R.string.info), R.drawable.ic_grid_info_white));
         mDrawerListItems.add(new DrawerItem(getString(R.string.help), R.drawable.ic_grid_info_white));
         mDrawerListItems.add(new DrawerItem(getString(R.string.logout), R.drawable.ic_grid_info_white));
-
-
 
 
     }
@@ -263,7 +261,7 @@ public class DrawerActivity extends AppCompatActivity implements BleStateListene
         mDrawerLayout.setDrawerShadow(R.drawable.drawer_shadow, GravityCompat.START);
         // set up the drawer's list view with items and click listener
         mDrawerListView = (ListView) findViewById(R.id.drawer_listview);
-        mDrawerListView.setAdapter(new NavDrawerAdapter(this, R.layout.list_item_drawer, mDrawerListItems,mDrawerListView));
+        mDrawerListView.setAdapter(new NavDrawerAdapter(this, R.layout.list_item_drawer, mDrawerListItems, mDrawerListView));
         mDrawerListView.setOnItemClickListener(new ListView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
@@ -276,6 +274,7 @@ public class DrawerActivity extends AppCompatActivity implements BleStateListene
                 }*/
 
                 showFragment(AppUtils.fragmentList[position]);
+                //Toast.makeText(getBaseContext(), AppUtils.fragmentList[position], Toast.LENGTH_LONG).show();
 
                 // Toast.makeText(getApplicationContext(), AppUtils.fragmentList[position] + "", Toast.LENGTH_LONG).show();
             }
