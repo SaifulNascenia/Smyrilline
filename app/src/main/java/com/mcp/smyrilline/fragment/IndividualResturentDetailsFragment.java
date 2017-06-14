9656642e077e16fb1a81423fff4f5b82640cbc4c
saiful <saiful@bdipo.com>
2017-06-14 18:42:21 +0600

    add view for restaturent detials

---
+++ app/src/main/java/com/mcp/smyrilline/fragment/IndividualResturentDetailsFragment.java
@@ -0,0 +1,30 @@
+package com.mcp.smyrilline.fragment;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.mcp.smyrilline.R;
+
+/**
+ * Created by saiful on 6/14/17.
+ */
+
+//https://stackoverflow.com/questions/36100187/how-to-start-fragment-from-an-activity
+public class IndividualResturentDetailsFragment extends Fragment {
+
+    private View _rootView;
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+
+        _rootView = inflater.inflate(R.layout.fragment_restaurent_details, container, false);
+
+
+        return _rootView;
+    }
+}
