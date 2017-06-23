a8ad614d4a931a8abc4fb93db086a769a6c4fa58
saiful <saiful@bdipo.com>
2017-06-23 13:02:49 +0600

    solve expanded textview bug and set new logic for expanded textview

---
+++ app/src/main/java/com/mcp/smyrilline/interfaces/BindDataWithView.java
@@ -0,0 +1,11 @@
+package com.mcp.smyrilline.interfaces;
+
+/**
+ * Created by saiful on 6/23/17.
+ */
+
+public interface BindDataWithView {
+
+
+    public void setDataOnProductDetailsTextViewWithExpandTextViewListener();
+}
