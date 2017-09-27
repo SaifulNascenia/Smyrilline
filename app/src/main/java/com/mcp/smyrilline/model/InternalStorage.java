da7761924cee28f1847ba85f5f26649e2f22ac31
nicknabdullah <nickn.abdullah@gmail.com>
2017-09-27 13:53:27 +0600

    Update file read/write - return null on exception

--- app/src/main/java/com/mcp/smyrilline/model/InternalStorage.java
+++ app/src/main/java/com/mcp/smyrilline/model/InternalStorage.java
@@ -1,10 +1,8 @@
 package com.mcp.smyrilline.model;
 
 import android.content.Context;
-
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
-import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 
@@ -16,19 +14,29 @@ public final class InternalStorage {
     private InternalStorage() {
     }
 
-    public static void writeObject(Context context, String key, Object object) throws IOException {
-        FileOutputStream fos = context.openFileOutput(key, Context.MODE_PRIVATE);
+    public static void writeObject(Context context, String key, Object object) {
+        FileOutputStream fos;
+        try {
+            fos = context.openFileOutput(key, Context.MODE_PRIVATE);
+
         ObjectOutputStream oos = new ObjectOutputStream(fos);
         oos.writeObject(object);
         oos.close();
         fos.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
-    public static Object readObject(Context context, String key) throws IOException,
-            ClassNotFoundException {
-        FileInputStream fis = context.openFileInput(key);
-        ObjectInputStream ois = new ObjectInputStream(fis);
-        Object object = ois.readObject();
+    public static Object readObject(Context context, String key) {
+        Object object = null;
+        try {
+            FileInputStream fis = context.openFileInput(key);
+            ObjectInputStream ois = new ObjectInputStream(fis);
+            object = ois.readObject();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
         return object;
     }
 }
